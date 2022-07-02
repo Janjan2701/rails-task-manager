a056a73058d4363d6d492a22219d0f738230867e
january amanda january.amanda@gmail.com <january.amanda@gmail.com>
2022-07-02 16:42:36 +0900

    CRUD in Rails done

--- config/routes.rb
+++ config/routes.rb
@@ -4,20 +4,20 @@ Rails.application.routes.draw do
 	# verb 'path', to: 'controller#action'
 
   # Read all tasks
-  get 'tasks', to: 'tasks#index' , as: 'tasks'
-
-  # Read one task
-  get 'tasks/:id', to: 'tasks#show'
+  get 'tasks', to: 'tasks#index', as: 'tasks'
 
   # Create the task
   #   1. Display the form
-  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
   #   2. Create the task
-  post 'tasks', to: 'posts#create'
+  post 'tasks', to: 'tasks#create'
+
+  # Read one task
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 
   # Update the task
   #   1. Display the form
-  get 'tasks/:id/edit', to: 'tasks#edit'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   #   2. Update the task
   patch 'tasks/:id', to: 'tasks#update'
 
--- db/schema.rb
+++ db/schema.rb
@@ -19,5 +19,4 @@ ActiveRecord::Schema.define(version: 2022_07_02_024812) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
